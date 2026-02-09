2d1ae4003ad2de42219497fa1d5cdd000f8a6b98
Omar Chiruco <chirucoomar90@gmail.com>
2026-02-09 10:04:44 +0200

    com

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//JavaScript code for NextGen True Light Education Center website
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 4000);
+
+function toggleMenu() {
+  document.getElementById('navMenu').classList.toggle('show');
+}
+
+function showMore() {
+  document.getElementById('primary').innerHTML += `
+    <div class="subject"><img src="images/ingles.png"><p>Inglês</p></div>
+    <div class="subject"><p>Educação Moral e Cívica</p></div>
+    <div class="subject"><p>Educação Visual e Ofícios</p></div>
+  `;
+  document.getElementById('moreBtn').style.display = 'none';
+}
+// SLIDER BOAS-VINDAS
+let welcomeSlides = document.querySelectorAll('.welcome-slide');
+let welcomeIndex = 0;
+
+setInterval(() => {
+  welcomeSlides[welcomeIndex].classList.remove('active');
+  welcomeIndex = (welcomeIndex + 1) % welcomeSlides.length;
+  welcomeSlides[welcomeIndex].classList.add('active');
+}, 4000); // muda a cada 4 segundos
